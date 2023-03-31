11d7209f99a2c70797553e6e8d61d5742f82e567
Rodevp <rodriguezrodrigor12@gmail.com>
2023-03-31 17:40:29 -0500

    convencion de rest

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const PORT = 3000
 const productsRouter = require('./routes/messages.js')
 
+//middlewares
+app.use( express.json() )
+
 //los endpoints dinamicos deben ir al final, los especificos deben ir al inicio
 
 //GET
@@ -26,6 +29,37 @@ app.get('/posts', (req, res) => {
 
 
 })
+
+
+//POST
+app.post('/user', (req, res) => {
+    
+    const body = req.body
+
+    res.json({
+        message: 'created',
+        data: body
+    })
+
+})
+
+//Update -> envia todos los atributos para hacer partial
+app.put('/user/:id', (req, res) => {
+    //actualización completa
+})
+
+//patch -> actualizacion parcial
+app.patch('/user/:id', (req, res) => {
+    //logica para actualización parcial
+})
+
+//delete -> eliminar
+app.delete('/user/:id', (req, res) => {
+    //logica para eliminar
+})
+
+
+
 
 //routes separados
 app.use('/messages', productsRouter)
